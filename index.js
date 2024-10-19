7663f5022dd045d69f7f629a36b71c770e3e7102
Oadtodev <e59704156@gmail.com>
2024-10-20 01:12:31 +0700

    index.js

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ app.set('view engine', 'ejs');
 // MySQL Connection
 const db = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'mydb'
+    user: 'satawatsingwijarn',
+    password: '0UnbAeSldGyg6MYcA1IiPIPNeyPNDgqq',
+    database: 'mydb_5c4b'
 });
 
 db.connect((err) => {
